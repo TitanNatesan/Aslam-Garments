907dc14a29ea0a76ade767331045e8abc263a335
gokulmanikandan35 <gokulmanikandan230@gmail.com>
2025-01-22 19:40:46 +0530

    versel fix

--- AslamGarmentsF/next.config.mjs
+++ AslamGarmentsF/next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-<<<<<<< HEAD
-      domains: ['localhost', 'titandev.me', '192.168.141.25', "robohash.org"], // Add both 'localhost' and 'titandev.me' or any other domain as needed
-=======
-      domains: ['localhost', 'titandev.me',"robohash.org","192.168.21.25"], // Add both 'localhost' and 'titandev.me' or any other domain as needed
->>>>>>> 5a3f33a061b78ef188cee03f55d542a38d7926a0
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["localhost", "titandev.me", "192.168.141.25", "robohash.org"], // Add both 'localhost' and 'titandev.me' or any other domain as needed
+  },
+};
+export default nextConfig;
--- AslamGarmentsF/src/app/utils/Url.jsx
+++ AslamGarmentsF/src/app/utils/Url.jsx
@@ -1,7 +1,2 @@
 // export const baseurl = "https://titandev.me";
-<<<<<<< HEAD
-export const baseurl = "http://192.168.141.25:8000";
-=======
-// export const baseurl = "http://localhost:8000";
-export const baseurl = "http://192.168.21.25:8000/";
->>>>>>> 5a3f33a061b78ef188cee03f55d542a38d7926a0
+export const baseurl = "http://192.168.21.25:8000";
